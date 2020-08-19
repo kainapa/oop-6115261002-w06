df92b1f94fc10aebb00c0bbae25b455720f202f0
kainapa <68212816+kainapa@users.noreply.github.com>
2020-08-19 18:30:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace oop_6115261002_w06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("kainapa", " ", 15000, "Lecturer");
+            Person p2 = new Person("bnyeam", "", 20000, "Lecturer");
+            Person p3 = new Person("pea", "", 30000, "Staff");
+            Faculty f1 = new Faculty("Faculty of Educational");
+            f1.setPerson(p1, 0);
+            f1.setPerson(p2, 1);
+            f1.setPerson(p3, 2);
+            f1.printAll();
+        }
+    }
+}
